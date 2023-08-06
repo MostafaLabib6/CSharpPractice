2c96dbfe38aacb69ae331d9ecf3599f2b8308fb6
MostafaLabib6 <mostafalabib510@gmail.com>
2023-08-07 02:06:44 +0300

    Create FileSearcher.cs

---
+++ Events-Practice/FileSearcher.cs
@@ -0,0 +1,25 @@
+﻿public class FileSearcher{ 
+    
+    // Event
+    public event EventHandler<FileSearcherEventArgs>? OnSearchCompleted;
+
+    public void Search(string? directory,string filePattern){ 
+        
+        foreach (var file in Directory.EnumerateFiles(directory,filePattern)){ 
+            
+            FileSearcherEventArgs e = new(file);
+
+            if(e.Cancelled)
+                break;
+
+            OnFileFound(e);
+        }
+    }
+
+    protected virtual void OnFileFound(FileSearcherEventArgs fileSearcherEventArgs)
+    {
+        OnSearchCompleted?.Invoke(this, fileSearcherEventArgs);
+        
+    }
+
+}

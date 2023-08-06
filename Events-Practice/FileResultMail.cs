f9d77eb3115cba5aca0be70898314396c436f357
MostafaLabib6 <mostafalabib510@gmail.com>
2023-08-07 02:08:20 +0300

    Create FileResultMail.cs

---
+++ Events-Practice/FileResultMail.cs
@@ -0,0 +1,32 @@
+﻿
+FileSearcher fileSearcher = new();
+SearchResultMail searchResultMail = new();
+SearchResultMessage searchResultMessage = new();
+
+fileSearcher.OnSearchCompleted += searchResultMail.OnFileFound;
+fileSearcher.OnSearchCompleted += searchResultMail.OnFileFound;
+
+
+fileSearcher.Search(@"test","*.txt");
+
+
+
+
+
+
+public class SearchResultMail
+{
+    public void OnFileFound(object source, FileSearcherEventArgs fileSearcherEventArgs)
+    {
+
+        // Sending mail
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Sending Mail...");
+        Thread.Sleep(2000);
+
+
+        Console.WriteLine($"Files:{fileSearcherEventArgs.FileName} \n");
+
+    }
+}

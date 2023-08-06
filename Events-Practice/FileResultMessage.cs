641f47873053d6e5ca03b738c710c4bca643c68c
MostafaLabib6 <mostafalabib510@gmail.com>
2023-08-07 02:06:49 +0300

    Create FileResultMessage.cs

---
+++ Events-Practice/FileResultMessage.cs
@@ -0,0 +1,15 @@
+﻿public class SearchResultMessage
+{
+    public void OnFileFound(object source, FileSearcherEventArgs fileSearcherEventArgs)
+    {
+
+        // Display Message
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Display a Message...");
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"Files:{fileSearcherEventArgs.FileName} \n");
+
+    }
+}
